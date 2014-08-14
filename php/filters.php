5f2f674cab91a33f852c755bcda0dd1b7d3ed842
phong <media@phong.com>
2014-08-14 13:56:34 -0700

    dev

--- php/filters.php
+++ php/filters.php
@@ -12,69 +12,81 @@ function i_meta_postmeta_defaults( $post ){
 	$default_i_meta = i_get_obj( $iOptions, 'posts.post.post_meta.i_meta' );
 
 	// If default i_meta is set
-	if( !empty( $default_i_meta ) ){
-		
-		// Use default meta as default values
-		$post['post_meta']['i_meta'] = array_replace_recursive(
-			$default_i_meta,
-			$post['post_meta']['i_meta']
-			);
-		
-		////////// SET DEFAULT VALUES //////////
-		
-		///// DOWNLOAD IMAGE /////
-		$post_value = i_get_obj( $post, 'post_meta.i_meta.image.download' ); // $post['post_meta']['i_meta']['image']['download'];
-		$site_value = i_get_obj( $default_i_meta, 'image.download' );
-		// Override 'default' value with the default value
-		if( $post_value == 'default' && $mode != 'edit' ){
-			// Set in post
-			$post['post_meta']['i_meta']['image']['download'] = $site_value;
-		}
-		
-		///// LABEL : SHOW /////
-		$post_obj = i_get_obj( $post, 'post_meta.i_meta.link_url.label' );
-		$site_obj = i_get_obj( $default_i_meta, 'link_url.label' );
-		$new_obj = array();
-		switch( $post_obj['show'] ){
-			case 'default':
-				$new_obj = $site_obj;
-				break;
-
-			case 'custom':
-				$new_obj = $post_obj;
-				break;
-
-			case true:
-					// Use site defaults
-					$new_obj = $site_obj;
-					// Force show true
-					$new_obj['show'] = true;
-				break;
-				
-			case false:
-				// Use site defaults
-				$new_obj = $site_obj;
-				// Force show false
-				$new_obj['show'] = false;
-				break;
-
-		}
-
-		///// LABEL : HIGHLIGHT /////
-		switch( $post_obj['highlight'] ){
-			case 'default':
-				// Use the site default value
-				$new_obj['highlight'] = $site_obj['highlight'];
-				break;
-		}
-
-		// Set the decided values
-		$post['post_meta']['i_meta']['link_url']['label'] = $new_obj;
-
+	if( empty( $default_i_meta ) )
+		return $post;
+	
+	// Use default meta as default values
+	$post['post_meta']['i_meta'] = array_replace_recursive(
+		$default_i_meta,
+		$post['post_meta']['i_meta']
+		);
+	
+	//////////////////// SET DEFAULT VALUES ////////////////////
+	
+	////////// DOWNLOAD IMAGE //////////
+	$post_value = i_get_obj( $post, 'post_meta.i_meta.image.download' ); // $post['post_meta']['i_meta']['image']['download'];
+	$site_value = i_get_obj( $default_i_meta, 'image.download' );
+	// Override 'default' value with the default value
+	if( $post_value == 'default' && $mode != 'edit' ){
+		// Set in post
+		$post['post_meta']['i_meta']['image']['download'] = $site_value;
 	}
 	
-	//$post = i_set_obj( $post, "post_meta.i_meta.defaults", $default_i_meta );
+	////////// LINK URL //////////
+	///// LABEL : SHOW /////
+	$post_obj = i_get_obj( $post, 'post_meta.i_meta.link_url' );
+	$site_obj = i_get_obj( $default_i_meta, 'link_url' );
+	$new_obj = $post_obj;
+	switch( $post_obj['label']['show'] ){
+		case 'default':
+			$new_obj['label'] = $site_obj['label'];
+			break;
 
+		case 'custom':
+			$new_obj['label'] = $post_obj['label'];
+			break;
+
+		case true:
+				// Use site defaults
+				$new_obj['label'] = $site_obj['label'];
+				// Force show true
+				$new_obj['label']['show'] = true;
+			break;
+
+		case false:
+			// Use site defaults
+			$new_obj['label'] = $site_obj['label'];
+			// Force show false
+			$new_obj['label']['show'] = false;
+			break;
+
+	}
+
+	///// LABEL : HIGHLIGHT /////
+	switch( $post_obj['label']['highlight'] ){
+		case 'default':
+			// Use the site default value
+			$new_obj['label']['highlight'] = $site_obj['label']['highlight'];
+			break;
+	}
+
+	///// NEW TARGET /////
+	$new_obj['new_target'] = $post_obj['new_target'];
+
+	switch( $post_obj['new_target'] ){
+		case 'default':
+			// Use the site default value
+			if( !is_bool( $post_obj['new_target'] ) ){
+				$new_obj['new_target'] = $site_obj['new_target'];
+			}
+			break;
+	}
+
+	// Set values into the post
+	$post['post_meta']['i_meta']['link_url'] = $new_obj;
+
+	///// RETURN /////	
+	//$post = i_set_obj( $post, "post_meta.i_meta.defaults", $default_i_meta );
 	return $post;	
 
 }
