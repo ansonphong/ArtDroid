64026d69ec8ae822acaff375f77ceeb290673bde
phong <media@phong.com>
2014-10-22 14:49:31 -0700

    fixes

--- views/archive/index.php
+++ views/archive/index.php
@@ -1,35 +1,17 @@
-<!-- INFINITE HEADER -->
-<?php
-	i_header();
-	global $post;
-?>
-<?php
-////////// PAGE ////////// ?>
+<?php i_header(); ?>
 <div id="page" class="page-home">
 	<?php
-	////////// HEAD //////////
 	include locate_template( 'views/modules/page-head.php' ); ?>
-	<?php
-	////////// CONTENT ////////// ?>
 	<div id="content" class="layout full page-bounds">
 		<?php
-			///////// INFINITE LAYOUT /////////
-			// Callback function for the page content (optional)
-			function page_content_function(){
-			}
-			///// MAIN LAYOUT /////
-			global $iGlobals;
 			$layout_args = array(
-				//'function'			=>	'page_content_function',
 				'content'			=>	pw_ob_include_template('views/archive/feed.php'),
 				'before_content' 	=>	'<div class="post_content">',
 				'after_content' 	=>	'</div>',
-				
 				);
 			i_print_layout( $layout_args );
 		?>
-
 	</div>
 </div>
-<!-- INFINITE FOOTER -->
+
 <?php i_footer(); ?>
