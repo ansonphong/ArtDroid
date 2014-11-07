8fc912919cc2e5b5ab221d9d80a0cbc70c6d538d
phong <media@phong.com>
2014-11-06 18:20:41 -0800

    dev styles

--- views/header/artdroid-header.php
+++ views/header/artdroid-header.php
@@ -4,8 +4,8 @@
 global $i_paths;
 global $pw;
 //echo json_encode($pw);
-
 ?>
+
 <!DOCTYPE html>
 <html  <?php language_attributes(); ?>>
 <head>
@@ -36,51 +36,52 @@ global $pw;
 	i-pointer-activate="addClass('active')"
 	inactive-delay="3000"
 	-->
+	<div class="page-width">
 
-	<div class="header-row">
-		<div id="logo" class="header-col-logo">
-			<?php
-			// Link to home page
-			if( !is_front_page() ){ ?>
-				<a
-					href="<?php echo get_home_url();?>"
-					tooltip="Go to Home Page"
-					tooltip-placement="bottom"
-					tooltip-popup-delay="1000">
-			<?php } ?>
-				<img src="<?php echo pw_get_image_option( array( 'option_name' => PW_OPTIONS_THEME, 'key' => 'images.logo', 'size' => 'full' ) )['url']; ?>">
-			<?php
-			// End link to home page
-			if( !is_front_page() ){ ?>
-				</a>
-			<?php } ?>
+		<div class="header-row">
+			<div id="logo" class="header-col-logo">
+				<?php
+				// Link to home page
+				if( !is_front_page() ){ ?>
+					<a
+						href="<?php echo get_home_url();?>"
+						tooltip="Go to Home Page"
+						tooltip-placement="bottom"
+						tooltip-popup-delay="1000">
+				<?php } ?>
+					<img src="<?php echo pw_get_image_option( array( 'option_name' => PW_OPTIONS_THEME, 'key' => 'images.logo', 'size' => 'full' ) )['url']; ?>">
+				<?php
+				// End link to home page
+				if( !is_front_page() ){ ?>
+					</a>
+				<?php } ?>
+			</div>
+			
+			<div id="main-menu" class="header-col-menu">
+				<?php include locate_template( 'views/menus/main-menu.php' ); ?>
+			</div>
+
+			<div id="mobile-menu">
+
+				<button
+					class="mobile-menu"
+					pw-click-toggle-display="#main-menu"
+					pw-click-toggle-class="#mobile-menu .mobile-menu"
+					toggle-class="selected">
+					<i class="icon-nav"></i>
+				</button>
+
+			</div>
+
 		</div>
-		
-		<div id="main-menu" class="header-col-menu">
-			<?php include locate_template( 'views/menus/main-menu.php' ); ?>
-		</div>
-
-		<div id="mobile-menu">
-
-			<button
-				class="mobile-menu"
-				pw-click-toggle-display="#main-menu"
-				pw-click-toggle-class="#mobile-menu .mobile-menu"
-				toggle-class="selected">
-				<i class="icon-nav"></i>
-			</button>
-
-		</div>
-
 	</div>
-
 	<div class="clearfix"></div>
 
 </header>
 
 <div
 	id="content"
-	class="page-content i-transition-fadein"
+	class="page-content i-transition-fadein page-width"
 	pw-timeout="100"
 	timeout-action="addClass('fadein-on')">
 	
