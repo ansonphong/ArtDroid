d78528cfa1d5e473cd73e4b487a7fcd3a0e723a0
phong <media@phong.com>
2014-11-20 10:34:51 -0500

    dev

--- views/archive/feed.php
+++ views/archive/feed.php
@@ -1,11 +1,9 @@
 <?php
 	global $pw;
 	//echo json_encode( $pw['view']['context'] );
-?>
+	$view_context = $pw['view']['context'];
 
-<!-- ////////// FEED ////////// -->
-<?php
-
+	//////////// FEED //////////
 	// Setup Query
 	$feed_query = array(
 		'post_type'			=>	'post',
@@ -14,28 +12,20 @@
 		'posts_per_page'	=>	500,
 		);
 
-	switch( $pw['view']['type'] ){
-
-		case 'archive-taxonomy':
-			$feed_query['tax_query'] = array(
-				array(
-					'taxonomy'	=>	$pw['view']['term']['taxonomy'],
-					'field'		=>	'id',
-					'terms'		=>	$pw['view']['term']['term_id']
-					)
-				);
-			break;
-
-		// Add date query
-		case 'archive-year';
-		case 'archive-month';
-		case 'archive-day';
-			$feed_query = array_replace_recursive($pw['view']['query'], $feed_query);
-			break;
-
+	/// DATE ARCHIVE ///
+	if( in_array( 'archive-date', $view_context ) ){
+		$feed_query = array_replace_recursive($pw['view']['query'], $feed_query);
 	}
-
-	//echo json_encode($feed_query);
+	/// TAXONOMY ARCHIVE ///
+	else if( in_array( 'archive-taxonomy', $view_context ) ){
+		$feed_query['tax_query'] = array(
+			array(
+				'taxonomy'	=>	$pw['view']['term']['taxonomy'],
+				'field'		=>	'id',
+				'terms'		=>	$pw['view']['term']['term_id']
+				)
+			);
+	}
 
 	// Setup Feed
 	$feed_vars = array(
@@ -54,13 +44,4 @@
 		
 	// Insert Live Feed
 	pw_live_feed( $feed_vars );
-?>
-
-
-<!-- ////////// END FEED ////////// -->
-
-<?php
-	///// DEV /////
-	//echo $pw['view']['term'];
-	//echo "<pre>pw : " . json_encode( $pw, JSON_PRETTY_PRINT ) . "</pre>";
 ?>
