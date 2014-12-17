00c1564c611e85598f96a9526865f30e7524373a
phong <media@phong.com>
2014-12-16 21:40:32 -0500

    dev

--- views/header/artdroid-header.php
+++ views/header/artdroid-header.php
@@ -12,7 +12,7 @@ global $pw;
 	<base href="/">
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1" />
-	<title><?php wp_title(' | ', true, 'right'); ?></title>
+	<title><?php wp_title(' | ', true, 'left'); ?></title>
 	<link rel="icon" type="image/png" href="<?php echo pw_site_favicon()['url']; ?>">
 	<?php wp_head(); ?>
 </head>
