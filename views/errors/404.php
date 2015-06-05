8ca6b4dc206716e51e875789bccdf8727cdcd01d
phong <media@phong.com>
2015-06-05 19:12:33 -0400

    add error page

--- views/errors/404.php
+++ views/errors/404.php
@@ -1,6 +1,14 @@
 <div class="error-page error-404">
+	
 	<div class="notice well">
-		<h1>Page Not Found</h1>
+		<h1>Not Found</h1>
 		<h3>404</h3>
+	
+		<a href="<?php echo get_site_url(); ?>">
+			<button class="btn-lg transparent btn-primary-color btn-outline">
+				Continue <i class="pwi-arrow-right-thin"></i>
+			</button>
+		</a>
+
 	</div>
 </div>
