a505317cc2098f008443d1e791b37236f72d7223
phong <media@phong.com>
2015-07-23 22:57:44 -0400

    dev

--- views/shortcodes/subpages.php
+++ views/shortcodes/subpages.php
@@ -12,6 +12,7 @@
 
 <div class="pw-shortcode subpages feed <?php echo $vars['size'] ?> <?php echo $vars['class'] ?>">
 	<?php
+		//pw_log( 'feed', $vars['feed'] );
 		echo pw_print_feed( $vars['feed'] );	
 	?>
 </div>
