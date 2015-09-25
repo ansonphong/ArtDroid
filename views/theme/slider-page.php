a7e9e8c25ae440ca027be199fadf87581e996a5e
phong <media@phong.com>
2015-09-24 21:51:37 -0700

    bugfixes, template enhancements WIP

--- views/theme/slider-page.php
+++ views/theme/slider-page.php
@@ -1,13 +1,14 @@
 <div class="slider">
-	<?php
-		// Get the slider from post meta
-		$slider = $pw_meta_header['slider'];
+<?php
+	// Get the slider from post meta
+	$slider = $pw_meta_header['slider'];
 
-		// Set additional keys
-		$slider['template'] = 'slider-default';
-		$slider['id'] = 'header-slider';
-		$slider['class'] = 'slider';
+	// Set additional keys
+	$slider['template'] = 'slider-default';
+	$slider['id'] = 'header-slider';
+	$slider['class'] = 'slider';
 
-		echo pw_print_slider( $slider );
-	?>
+	echo pw_print_slider( $slider );
+
+?>
 </div>
