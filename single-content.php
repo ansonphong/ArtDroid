7c95c9d840e2c40611b7f9bf44572cffbcb6659b
phong <media@phong.com>
2015-10-12 23:10:33 -0700

    add support for password protected content

---
+++ single-content.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * ///// CONTENT /////
+ * The main part of the post.
+ */
+
+// Social Media Widgets
+global $social_settings;
+$social_settings['meta']['url'] = get_permalink();
+
+$fields = array(
+	'ID',
+	'post_id',
+	'post_type',
+	'post_status',
+	'post_title',
+	'post_content',
+	'post_excerpt',
+	'post_name',
+	'post_permalink',
+	'post_date',
+	'post_date_gmt',
+	'post_timestamp',
+	'post_class',
+	'post_format',
+	'link_format',
+	'link_url',
+	'related_post',
+	'post_parent',
+	'parent_post(preview)',
+	'image(stats)',
+	'image(tags)',
+	'image(sm,512,512,2)',
+	'image(md,1024,1024,2)',
+	'image(lg,2048,2048,2)',
+	'image(xl,4096,4096,2)',
+	'image(full)',
+	'time_ago',
+	'author(ID)',
+	'edit_post_link',
+	'taxonomy(all)',
+	'post_meta(all)',
+	'gallery(ids,posts)'
+	);
+
+///// PRINT POST /////
+$post_settings = array(
+	'post_id'	=>	$post->ID,
+	'fields'	=>	$fields,
+	'view'		=>	'full-h2o',
+	'vars'	=> array(
+		'social_widgets'	=>	pw_social_widgets( $social_settings ),
+		'social_share'		=>	pw_social_share( $post ),
+		'pw'				=>	$pw,
+		'password_protected'=> !empty( $post->post_password ),
+		),
+	'js_vars'	=>	array('post'),
+	);
+
+// Get and Print the Post in Template
+$post_html = pw_print_post( $post_settings );
