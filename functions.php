6217f68bc5bdee173eca639985b13d288abbeb8e
phong <media@phong.com>
2015-11-17 21:01:56 -0800

    dev

--- functions.php
+++ functions.php
@@ -302,4 +302,26 @@ function theme_mc4wp_form_action( $action ){
 }
 add_filter( 'mc4wp_form_action', 'theme_mc4wp_form_action' );
 
+
+/**
+ * From Plugin: Private Content Login Redirect
+ * Plugin URI: http://increasy.com
+ * Redirects non-logged users to the login page when they follow a link to private post or private page.After successful login, it automatically redirects users with private content access to the private content link they followed.
+ * Author: Kumar Abhisek
+ * Author URI: http://increasy.com
+ */
+add_action('template_redirect', 'theme_private_content_redirect_to_login', 9);
+function theme_private_content_redirect_to_login() {
+	global $wp_query,$wpdb;
+	if (is_404()) {
+		$private = $wpdb->get_row($wp_query->request);
+		$location = wp_login_url($_SERVER["REQUEST_URI"]);
+		if( 'private' == $private->post_status  ) {
+			wp_safe_redirect($location);
+			exit;
+		}
+	}
+}
+
+
 ?>
