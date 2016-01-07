8665255f30f2324ad0ffde49ce55d5624cff34c0
phong <media@phong.com>
2016-01-07 14:28:11 -0800

    dev

--- views/theme/header-search.php
+++ views/theme/header-search.php
@@ -5,7 +5,7 @@ if( $show_search ) : ?>
 		class="search-menu--inner">
 		<button
 			class="search-icon"
-			ng-click="uiToggleView( 'searchInput' ); uiFocusElement( '#search-menu-input-s', 250 )"
+			ng-click="uiToggleView( 'searchInput' ); uiFocusElement( '#search-menu-input-s', 333 )"
 			ng-class="uiSetClass( 'searchInput', 'active' )">
 			<i class="pwi-search"></i>
 		</button>
