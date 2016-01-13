3ba2e79c30a366765853c175f6f929795bac9248
phong <media@phong.com>
2016-01-13 10:16:45 -0800

    tweaks

--- php/postworld-filters.php
+++ php/postworld-filters.php
@@ -1,4 +1,22 @@
 <?php
+
+
+/**
+ * Preload Template Partials
+ * Increases page display speed.
+ * @todo place in postworld-actions.php file.
+ */
+add_action( 'theme_preload_templates', 'theme_preload_template_partials' );
+function theme_preload_template_partials(){
+	$context = pw_view_context();
+	if( in_array( 'archive-post-type-blog', $context ) ||
+		in_array( 'single-blog', $context ) ){
+		echo pw_grab_ng_template( 'panels', 'featured-image--full' );
+		echo pw_grab_ng_template( 'panels', 'featured-image--slice' );
+
+	}
+}
+
 /**
  * DEFAULT HEADER ID
  */
