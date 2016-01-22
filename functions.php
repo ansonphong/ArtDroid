54467296ddef54fe04fa201f606d5404926df927
phong <media@phong.com>
2016-01-21 19:29:55 -0800

    set update server

--- functions.php
+++ functions.php
@@ -171,16 +171,17 @@ add_action('wp_footer', 'pw_add_forms_action_attribute');
  */
 add_action('init','theme_check_for_updates');
 function theme_check_for_updates(){
+	global $theme_version;
 	require_once('lib/theme-updates/theme-update-checker.php');
-
-	$base_url = 'http://artdroid/';
+	$update_server_url = 'http://artdroid.phong.com/';
 	$query_vars = array(
 		'update_action' => 'get_metadata',
 		'update_slug' => 'artdroid',
-		'installed_version' => 1.333
+		'installed_version' => $theme_version
 		);
 	$query_string = http_build_query( $query_vars );
-	$request_url = $base_url . '?' . $query_string;
+	$request_url = $update_server_url . '?' . $query_string;
+	//pw_log('request_url', $request_url);
 	$update_theme = new ThemeUpdateChecker( 'artdroid', $request_url );
 }
 
