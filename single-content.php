c9f63d391c0373169fe844c419920b15315905a0
phong <media@phong.com>
2016-02-08 19:07:09 -0800

    dev comments

--- single-content.php
+++ single-content.php
@@ -53,7 +53,7 @@ $post_settings = array(
 		'header_type'			=>	$header_meta['type'],
 		'is_page_header_image' 	=>	( $header_meta['type'] === 'featured_image' && $post->post_type === 'page' ),
 		'device' 				=>	pw_device_meta(),
-		'comments'				=>	pw_get_comments_thirdparty( array( 'post_id' => $post->ID ) ),
+		'comments_thirdparty'	=>	pw_get_comments_thirdparty( array( 'post_id' => $post->ID ) ),
 		'post_class'			=>	pw_post_class('', $post->ID),
 		),
 	'js_vars'	=>	array('post'),
