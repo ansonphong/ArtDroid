1a76446e44b2ce489cd6f5822a9f7738df3df526
phong <media@phong.com>
2016-02-09 21:52:47 -0800

    bugfix

--- views/galleries/gallery-inline.php
+++ views/galleries/gallery-inline.php
@@ -1,14 +1,11 @@
 <!-- POSTWORLD INLINE GALLERY : <?php echo $vars['instance']; ?> -->
 <script>
-	postworld.controller( '<?php echo $vars['instance']; ?>',
-		[ '$scope', '$_', '$pw', 'pwImages', 'pwPosts', 'pwModal', 'pwData',
-		function( $scope, $_, $pw, $pwImages, $pwPosts, $pwModal, $pwData ){
-		var instance = "<?php echo $vars['instance']; ?>";
-		var galleryInstance = "gallery-" + instance;
+	postworld.controller( '<?php echo $vars['instance']; ?>', function( $scope, $pwData ){
+		var galleryInstance = "gallery-<?php echo $vars['instance']; ?>";
 		var galleryPosts = <?php echo json_encode( $vars['posts'] ); ?>;
 		$pwData.insertFeed( galleryInstance, { posts: galleryPosts } );
 		$scope.feed = $pwData.getFeed( galleryInstance );
-	}]);
+	});
 	pwRegisterController( "<?php echo $vars['instance']; ?>" );
 </script>
 
