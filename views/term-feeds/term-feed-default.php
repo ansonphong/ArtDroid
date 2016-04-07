d1bbcf2cd234b11ee4c4ee4a6f6d66a7f384e001
phong <media@phong.com>
2016-04-06 23:06:14 -0700

    add gradient overlay to term feed grid

--- views/term-feeds/term-feed-default.php
+++ views/term-feeds/term-feed-default.php
@@ -4,13 +4,14 @@
    		$scope.termFeed = <?php echo json_encode($term_feed); ?>;
 	});
 </script>
-<div class="term-feed grid pw-shortcode" ng-controller="<?php echo $instance; ?>">
+<div class="term-feed term-feed--grid pw-shortcode" ng-controller="<?php echo $instance; ?>">
 	<div class="terms-row">
 		<div class="terms-col" ng-repeat="feedTerm in termFeed">
 			<div
 				class="feed-term">
 				<a ng-href="{{feedTerm.term.url}}">
 					<div class="feed-term-posts">
+						<div class="gradient-overlay"></div>
 						<div
 							class="feed-term-post"
 							ng-repeat="post in feedTerm.posts"
