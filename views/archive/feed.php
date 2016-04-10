a844bc6021a8fd1478bdfa037b9d7c0040574a77
phong <media@phong.com>
2016-04-09 18:50:29 -0700

    fix front page posts

--- views/archive/feed.php
+++ views/archive/feed.php
@@ -1,13 +1,11 @@
 <?php
-	global $pw;
-	$feed_query = $pw['query'];
-	$feed_query['has_password'] = false;
+global $pw;
+$feed_query = $pw['query'];
+$feed_query['has_password'] = false;
 
-	$feed_vars = array(
-		'aux_template'	=>	'seo-list',
-		'feed'	=>	array(
-			'query'	=>	$feed_query,
-			),
-		);
-
-	pw_feed( $feed_vars );
+pw_feed( array(
+	'aux_template'	=>	'seo-list',
+	'feed'	=>	array(
+		'query'	=>	$feed_query,
+		),
+	));
