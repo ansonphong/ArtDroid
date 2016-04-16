80279a342b6c14bf792baf3fb777b40ad191985f
phong <media@phong.com>
2016-04-15 23:33:10 -0700

    dev customizer

--- php/customize.php
+++ php/customize.php
@@ -40,25 +40,166 @@ function theme_remove_customizer_options( $wp_customize ) {
 /**
  * Add new Customizer options.
  */
-add_action('customize_register','theme_customize_register');
+add_action( 'customize_register', 'theme_customize_register' );
 function theme_customize_register( $wp_customize ) {
 
+	/**
+	 * PANELS
+	 */
 	/*
-	$wp_customize->add_panel();
-	$wp_customize->get_panel();
-	$wp_customize->remove_panel();
+	$wp_customize->add_panel( 'current_theme', array(
+	    'priority' => 10,
+	    'capability' => 'edit_theme_options',
+	    'theme_supports' => '',
+	    'title' => __( 'ArtDroid Theme', 'textdomain' ),
+	    'description' => __( 'Customize the theme.', 'textdomain' ),
+	) );
+	*/
+	
 
-	$wp_customize->add_section();
-	$wp_customize->get_section();
-	$wp_customize->remove_section();
+	/*****************************************
+	 * SECTION : HEADER
+	 *****************************************
+	 */
+	$wp_customize->add_section( 'theme_header', array(
+	    'priority' => 10,
+	    'capability' => 'edit_theme_options',
+	    'theme_supports' => '',
+	    'title' => __( 'Header', 'textdomain' ),
+	    'description' => '',
+	    //'panel' => 'current_theme',
+	) );
 
-	$wp_customize->add_setting();
-	$wp_customize->get_setting();
-	$wp_customize->remove_setting();
+	/**
+	 * MENU ALIGNMENT
+	 */
+	$subkey = 'menus.primary.alignment';
+	$wp_customize->add_setting( 'PW_OPTIONS_THEME['.$subkey.']', array(
+		'default' => pw_grab_option( PW_OPTIONS_THEME, $subkey ),
+		'type' => 'postworld',
+		'capability' => 'edit_theme_options',
+		'transport' => '',
+		//'sanitize_callback' => 'esc_url',
+	) );
+	$wp_customize->add_control( 'PW_OPTIONS_THEME['.$subkey.']', array(
+	    'type' => 'select',
+	    'priority' => 10,
+	    'section' => 'theme_header',
+	    'label' => __( 'Menu Position', 'postworld' ),
+	    'description' => __('Position of the primary menu.', 'postworld'),
+	    'choices'  => array(
+			'left'  => 'Left',
+			'right' => 'Right',
+			)
+	) );
 
-	$wp_customize->add_control();
-	$wp_customize->get_control();
-	$wp_customize->remove_control();
+
+	/**
+	 * SHOW SEARCH
+	 */
+	$subkey = 'search.show_search';
+	$wp_customize->add_setting( 'PW_OPTIONS_THEME['.$subkey.']', array(
+		'default' => pw_grab_option( PW_OPTIONS_THEME, $subkey ),
+		'type' => 'postworld',
+		'capability' => 'edit_theme_options',
+		'transport' => '',
+		//'sanitize_callback' => 'esc_url',
+	) );
+	$wp_customize->add_control( 'PW_OPTIONS_THEME['.$subkey.']', array(
+		'type' => 'checkbox',
+		'priority' => 10,
+		'section' => 'theme_header',
+		'label' => __( 'Show Search', 'postworld' ),
+		'description' => __('Show search icon in upper right.', 'postworld'),
+	) );
+
+
+	/**
+	 * SHOW SOCIAL MENU
+	 */
+	$subkey = 'menus.primary.show_social';
+	$wp_customize->add_setting( 'PW_OPTIONS_THEME['.$subkey.']', array(
+		'default' => pw_grab_option( PW_OPTIONS_THEME, $subkey ),
+		'type' => 'postworld',
+		'capability' => 'edit_theme_options',
+		'transport' => '',
+		//'sanitize_callback' => 'esc_url',
+	) );
+	$wp_customize->add_control( 'PW_OPTIONS_THEME['.$subkey.']', array(
+		'type' => 'checkbox',
+		'priority' => 10,
+		'section' => 'theme_header',
+		'label' => __( 'Show Social Menu', 'postworld' ),
+		'description' => __('Show social media icons (if defined in ArtDroid › Social)', 'postworld'),
+	) );
+
+
+	/**
+	 * SHOW ICONS AT TOP LEVEL
+	 */
+	$subkey = 'menus.primary.show_icons_top';
+	$wp_customize->add_setting( 'PW_OPTIONS_THEME['.$subkey.']', array(
+		'default' => pw_grab_option( PW_OPTIONS_THEME, $subkey ),
+		'type' => 'postworld',
+		'capability' => 'edit_theme_options',
+		'transport' => '',
+		//'sanitize_callback' => 'esc_url',
+	) );
+	$wp_customize->add_control( 'PW_OPTIONS_THEME['.$subkey.']', array(
+		'type' => 'checkbox',
+		'priority' => 10,
+		'section' => 'theme_header',
+		'label' => __( 'Show Icons on Top Level', 'postworld' ),
+		'description' => __('Show page icons on top level menu items.', 'postworld'),
+	) );
+
+	/**
+	 * SHOW ICONS IN SUBMENUS
+	 */
+	$subkey = 'menus.primary.show_icons_sub';
+	$wp_customize->add_setting( 'PW_OPTIONS_THEME['.$subkey.']', array(
+		'default' => pw_grab_option( PW_OPTIONS_THEME, $subkey ),
+		'type' => 'postworld',
+		'capability' => 'edit_theme_options',
+		'transport' => '',
+		//'sanitize_callback' => 'esc_url',
+	) );
+	$wp_customize->add_control( 'PW_OPTIONS_THEME['.$subkey.']', array(
+		'type' => 'checkbox',
+		'priority' => 10,
+		'section' => 'theme_header',
+		'label' => __( 'Show Icons in Submenus', 'postworld' ),
+		'description' => __('Show page icons in dropdown submenus.', 'postworld'),
+	) );
+
+
+	
+
+
+
+
+
+	/*
+	$wp_customize->add_setting( 'email_field_id', array(
+		'default' => '',
+		'type' => 'theme_mod',
+		'capability' => 'edit_theme_options',
+		'transport' => '',
+		'sanitize_callback' => 'sanitize_email',
+	) );
+
+	$wp_customize->add_control( 'email_field_id', array(
+	    'type' => 'email',
+	    'priority' => 10,
+	    'section' => 'theme_header',
+	    'label' => __( 'Email Field', 'textdomain' ),
+	    'description' => '',
+	) );
 	*/
 
+
 }
+
+
+
+
