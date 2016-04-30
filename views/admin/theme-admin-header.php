02ed94c9dfbcc323be21e50f23cea222d92ecf9c
phong <media@phong.com>
2016-04-29 21:24:27 -0700

    dev admin styles

--- views/admin/theme-admin-header.php
+++ views/admin/theme-admin-header.php
@@ -1,23 +1,21 @@
 <div id="theme-admin-header" class="postworld">
-	<div class="row">
-		<div class="col-md-6">
-			<h1>
-				<span class="artdroid-brand">
-					<a href="//artdroid.phong.com" target="_blank">
-						<img src="<?php echo get_template_directory_uri() ?>/images/artdroid-sticker-orange-128.png" title="ArtDroid" style="height: 64px; width: auto;">
-					</a> 
-				</span>
-				<span class="subhead">
-					Theme Settings
-				</span>
-				<span class="theme-version">
-					v<?php echo pw_theme_version() ?>
-				</span>
-			</h1>
+	<div class="row header-row">
+		<div class="col-md-8 header-col">
+			<div class="artdroid-brand">
+				<a href="//artdroid.phong.com" target="_blank">
+					<img src="<?php echo get_template_directory_uri() ?>/images/artdroid-sticker-orange-128.png" title="ArtDroid" style="height: 64px; width: 200px;">
+				</a> 
+			</div>
+			<div class="subhead">
+				Theme Settings
+			</div>
+			<div class="theme-version">
+				v<?php echo pw_theme_version() ?>
+			</div>
 		</div>
-		<div class="col-md-6" style="text-align:right;">
-			<div class="theme-info">
-				Spawned by <a href="http://androidjones.com" target="_blank">Android Jones</a> • Designed by <a href="http://phong.com" target="_blank">Phong</a>
+		<div class="col-md-4 col-credits header-col">
+			<div class="theme-credits">
+				Spawned by <a href="http://androidjones.com" target="_blank">Android Jones</a><br> Designed by <a href="http://phong.com" target="_blank">Phong</a>
 			</div>
 		</div>
 	</div>
