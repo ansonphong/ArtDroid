c8144901c81f2a6aefa9b0a2c3504eff14a9dd4a
phong <media@phong.com>
2016-05-03 22:45:57 -0700

    version type switch

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ add_action( 'postworld_register_theme', 'artdroid_register_theme' );
 function artdroid_register_theme(){
 	pw_register_theme( array(
 		'slug' => 'artdroid',
-		'version' => '1.425',
+		'version' => '1.4.25',
 		));
 };
 
